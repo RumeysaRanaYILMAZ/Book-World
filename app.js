b36635659ddf41687252bf6eb98462b5e05eddde
Brian Voong <bhlvoong@gmail.com>
2018-04-17 15:51:40 -0700

    Ep 1 Getting Started

---
+++ app.js
@@ -0,0 +1,22 @@
+// load our app server using express somehow....
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+
+app.use(morgan('combined'))
+
+app.get("/", (req, res) => {
+  console.log("Responding to root route")
+  res.send("Hello from ROOOOOT")
+})
+
+app.get("/users", (req, res) => {
+  var user1 = {firstName: "Stephen", lastName: "Curry"}
+  const user2 = {firstName: "Kevin", lastName: "Durant"}
+  res.json([user1, user2])
+})
+
+// localhost:3003
+app.listen(3003, () => {
+  console.log("Server is up and listening on 3003...")
+})
