3ac426943829d1f2c56f4ae4ec7aa4118baefdaa
Brian Voong <bhlvoong@gmail.com>
2018-04-21 01:03:36 -0700

    Ep 4 Refactor and MySQL Pooling

--- app.js
+++ app.js
@@ -12,72 +12,9 @@ app.use(express.static('./public'))
 
 app.use(morgan('short'))
 
-app.post('/user_create', (req, res) => {
-  console.log("Trying to create a new user...")
-  console.log("How do we get the form data???")
+const router = require('./routes/user.js')
 
-  console.log("First name: " + req.body.create_first_name)
-  const firstName = req.body.create_first_name
-  const lastName = req.body.create_last_name
-
-  const queryString = "INSERT INTO users (first_name, last_name) VALUES (?, ?)"
-  getConnection().query(queryString, [firstName, lastName], (err, results, fields) => {
-    if (err) {
-      console.log("Failed to insert new user: " + err)
-      res.sendStatus(500)
-      return
-    }
-
-    console.log("Inserted a new user with id: ", results.insertId);
-    res.end()
-  })
-})
-
-function getConnection() {
-  return mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    database: 'lbta_mysql'
-  })
-}
-
-app.get('/user/:id', (req, res) => {
-  console.log("Fetching user with id: " + req.params.id)
-
-  const connection = getConnection()
-
-  const userId = req.params.id
-  const queryString = "SELECT * FROM users WHERE id = ?"
-  connection.query(queryString, [userId], (err, rows, fields) => {
-    if (err) {
-      console.log("Failed to query for users: " + err)
-      res.sendStatus(500)
-      return
-      // throw err
-    }
-
-    console.log("I think we fetched users successfully")
-
-    const users = rows.map((row) => {
-      return {firstName: row.first_name, lastName: row.last_name}
-    })
-
-    res.json(users)
-  })
-})
-
-app.get("/users", (req, res) => {
-  const connection = getConnection()
-  const queryString = "SELECT * FROM users"
-  connection.query(queryString, (err, rows, fields) => {
-    if (err) {
-      console.log("Failed to query for users: " + err)
-      res.sendStatus(500)
-      return
-    }
-    res.json(rows)
-  })
-})
+app.use(router)
 
 app.get("/", (req, res) => {
   console.log("Responding to root route")
